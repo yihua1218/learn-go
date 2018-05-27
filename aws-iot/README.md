32de3305630eadede0352a7a141e66db23881b1c
Yi-Hua Liang <yihua1218@gmail.com>
2018-05-27 12:37:52 +0800

    Update main.go

--- aws-iot/README.md
+++ aws-iot/README.md
@@ -1,6 +1,13 @@
 # AWS IoT MQTT Client Example
 
+``` bash
+$ go get github.com/eclipse/paho.mqtt.golang
+$ go get golang.org/x/net/websocket
+```
 
 ## Reference
 
 1. [Pure Go MQTT Client](https://github.com/yosssi/gmq)
+2. [Eclipse Paho MQTT Go client](https://github.com/eclipse/paho.mqtt.golang)
+3. [Connecting to AWS IoT](https://github.com/eclipse/paho.mqtt.golang/issues/72)
+4. [wisebot-operator/iot/iot.go ](https://github.com/wisegrowth/wisebot-operator/blob/master/iot/iot.go#L138)
---
+++ aws-iot/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	cer, err := tls.LoadX509KeyPair("certs/cert.pem", "certs/private.key")
+	check(err)
+
+	cid := "ASUS"
+
+	// AutoReconnect option is true by default
+	// CleanSession option is true by default
+	// KeepAlive option is 30 seconds by default
+	connOpts := MQTT.NewClientOptions()
+	connOpts.SetClientID(cid)
+	connOpts.SetMaxReconnectInterval(1 * time.Second)
+	connOpts.SetTLSConfig(&tls.Config{Certificates: []tls.Certificate{cer}})
+
+	host := "avzi68bry4as0.iot.ap-northeast-1.amazonaws.com"
+	port := 8883
+	path := "/mqtt"
+
+	brokerURL := fmt.Sprintf("tcps://%s:%d%s", host, port, path)
+	connOpts.AddBroker(brokerURL)
+	connOpts.SetOnConnectHandler(onConnect())
+
+	mqttClient := MQTT.NewClient(connOpts)
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	log.Println("[MQTT] Connected")
+
+	quit := make(chan struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		mqttClient.Disconnect(250)
+		fmt.Println("[MQTT] Disconnected")
+
+		quit <- struct{}{}
+	}()
+	<-quit
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Reported struct {
+	InterfaceAddrs interface{}
+}
+
+type State struct {
+	reported Reported
+}
+
+type Message struct {
+	state State
+}
+
+func onConnect() MQTT.OnConnectHandler {
+
+	return func(client MQTT.Client) {
+		log.Println("Running MQTT.OnConnectHandler")
+		addrs, err := net.InterfaceAddrs()
+		if err != nil {
+			fmt.Println("err:", err)
+		} else {
+			fmt.Print("addrs:", addrs)
+		}
+		message := &Message{
+			state: State{
+				reported: Reported{
+					InterfaceAddrs: addrs,
+				},
+			},
+		}
+		payload, err := json.Marshal(message)
+
+		fmt.Println("payload:", payload)
+
+		// client.Publish("$aws/things/asus/shadow/update", byte(1), true, payload)
+	}
+
+}
