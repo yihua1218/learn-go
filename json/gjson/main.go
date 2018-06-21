f8223bc7d6578796ac8c536e7b857f8070a5c029
Yi-Hua Liang <yihua1218@gmail.com>
2018-06-21 08:33:49 +0800

    Updated .gitignore and example.

--- json/gjson/main.go
+++ json/gjson/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/tidwall/gjson"
 )
 
 func main() {
@@ -21,5 +23,24 @@ func main() {
 		os.Exit(3)
 	}
 
-	fmt.Printf("file: %v\n", file)
+	fmt.Printf("file: %s\n", file)
+
+	json := string(file)
+
+	root := gjson.Parse(json)
+	headers := gjson.Get(json, "headers")
+
+	fmt.Printf("headers: %v\n", headers)
+
+	requestContext := gjson.Get(string(file), "requestContext")
+
+	requestContext.ForEach(func(key, value gjson.Result) bool {
+		fmt.Printf("key: %s\n", key)
+		return true
+	})
+
+	root.ForEach(func(key, value gjson.Result) bool {
+		fmt.Printf("key: %s\n", key)
+		return true
+	})
 }
